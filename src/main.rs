0b30746ca5acd243b8e2e9d1cb8098c895fc6330
1mamute <gabrielbassinello@hotmail.com>
2024-02-07 14:21:56 -0300

    feat: helloworld windowed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use bevy::{app::{App, Startup, Update}, ecs::{component::Component, query::With, schedule::IntoSystemConfigs, system::{Commands, Query}}, DefaultPlugins};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .run();
 }
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+// This query reads as: iterate over every Name component for entities that also have a Person component
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Elaina Proctor" {
+            name.0 = "Elaina Hume".to_string();
+            break; // We don’t need to change any other names
+        }
+        println!("Not found Elaine Proctor!");
+    }
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
