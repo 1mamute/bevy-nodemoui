530dd7e833d30c28cb4a291b08f9ce15d2fe54cc
1mamute <gabrielbassinello@hotmail.com>
2024-02-08 19:31:02 +0000

    feat: diagnosticsplugin

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,43 @@
-use bevy::{app::{App, Plugin, Startup, Update}, ecs::{component::Component, query::With, system::{Commands, Query}}, DefaultPlugins};
-
+use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
+use bevy::diagnostic::LogDiagnosticsPlugin;
+use bevy::{
+    app::{App, Plugin, Startup, Update},
+    ecs::{
+        component::Component,
+        query::With,
+        system::{Commands, Query, Res, ResMut, Resource},
+    },
+    time::{Time, Timer, TimerMode},
+    DefaultPlugins,
+};
 pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_people)
-           .add_systems(Update, greet_people);
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, greet_people);
     }
 }
 
+#[cfg(debug_assertions)] // debug/dev builds only
+pub struct DiagnosticsPlugin;
+impl Plugin for DiagnosticsPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(FrameTimeDiagnosticsPlugin::default())
+            .add_plugins(LogDiagnosticsPlugin::default());
+    }
+}
 fn main() {
-    App::new()
-        .add_plugins((DefaultPlugins, HelloPlugin))
-        .run();
+    let mut app = App::new();
+    app.add_plugins((DefaultPlugins, HelloPlugin));
+
+    // Adiciona os plugins de diagnóstico apenas em modo debug
+    #[cfg(debug_assertions)]
+    {
+        app.add_plugins(DiagnosticsPlugin);
+    }
+
+    app.run();
 }
 
 fn add_people(mut commands: Commands) {
@@ -20,10 +46,21 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
-// This query reads as: iterate over every Name component for entities that also have a Person component
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_people(
+    time: Res<Time>,
+    mut timer: ResMut<GreetTimer>,
+    // This query reads as: iterate over every Name component for entities that also have a Person component
+    query: Query<&Name, With<Person>>,
+) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
