0ee7bc1c6d8b12a6179b4a8886fe0dc559fa78d6
1mamute <gabrielbassinello@hotmail.com>
2024-02-09 03:24:12 +0000

    feat: one button added

--- src/ui.rs
+++ src/ui.rs
@@ -4,34 +4,77 @@ pub struct UIPlugin;
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(WinitSettings::desktop_app())
-            .add_systems(Startup, setup);
+            .add_systems(Startup, setup)
+            .add_systems(Update, button_system);
     }
 }
 
-fn setup(mut commands: Commands, _asset_server: Res<AssetServer>) {
+const PRESSED_BUTTON: Color = Color::rgb(1.0, 0.65, 0.65);
+const HOVERED_BUTTON: Color = Color::rgb(0.8, 0.65, 0.65);
+const NORMAL_BUTTON: Color = Color::rgb(0.65, 0.65, 0.65);
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
 
-    // root node
+    // Root UI Node
     commands
         .spawn(NodeBundle {
             style: Style {
                 width: Val::Percent(100.0),
                 height: Val::Percent(100.0),
-                justify_content: JustifyContent::SpaceBetween,
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
                 ..default()
             },
             ..default()
         })
         .with_children(|parent| {
-            // left vertical fill (border)
-            parent.spawn(NodeBundle {
-                style: Style {
-                    width: Val::Px(600.),
-                    border: UiRect::all(Val::Px(2.)),
+            // For each map, create a button like this
+            parent
+                .spawn(ButtonBundle {
+                    style: Style {
+                        width: Val::Px(150.0),
+                        height: Val::Px(65.0),
+                        border: UiRect::all(Val::Px(5.0)),
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    background_color: NORMAL_BUTTON.into(),
                     ..default()
-                },
-                background_color: Color::rgb(0.65, 0.65, 0.65).into(),
-                ..default()
-            });
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Map Name",
+                        TextStyle {
+                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            font_size: 40.0,
+                            color: Color::WHITE,
+                        },
+                    ));
+                });
         });
+}
+
+fn button_system(
+    mut commands: Commands,
+    mut interaction_query: Query<
+        (&Interaction, &mut BackgroundColor, Entity),
+        (Changed<Interaction>, With<Button>),
+    >,
+) {
+    for (interaction, mut color, entity) in interaction_query.iter_mut() {
+        match *interaction {
+            Interaction::Pressed => {
+                *color = PRESSED_BUTTON.into();
+                commands.entity(entity).despawn_recursive();
+            }
+            Interaction::Hovered => {
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                *color = NORMAL_BUTTON.into();
+            }
+        }
+    }
 }
