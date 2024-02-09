412ae1d602b6853a857605a12060d292ed6934f3
1mamute <gabrielbassinello@hotmail.com>
2024-02-09 00:03:25 +0000

    feat: egui inspector

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ mod ui;
 
 use bevy::{app::App, DefaultPlugins};
 
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use diagnostics::DiagnosticsPlugin;
 use ui::UIPlugin;
 
 fn main() {
     let mut app = App::new();
     app.add_plugins((DefaultPlugins, UIPlugin));
+    app.add_plugins(WorldInspectorPlugin::new());
 
     // Adiciona os plugins de diagnóstico apenas em modo debug
     #[cfg(debug_assertions)]
